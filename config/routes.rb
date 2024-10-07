44990c7036b5f8fc112c9072e7039786cb9d44e0
Danissa <fonokit.2024@gmail.com>
2024-10-07 11:08:51 -0300

    Add attractive home page using Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "render#index"
+  root "movies#index"
 end
